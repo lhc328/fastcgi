3d32e53401ea1b7b91dbadf410e95f3471473474
lhc328 <32303880+lhc328@users.noreply.github.com>
2018-12-29 12:34:39 +0800

    sort modify

--- 01packet-re/main.cpp
+++ 01packet-re/main.cpp
@@ -78,13 +78,13 @@ void Sort(Object Q[], int n)
     int flag=0;
     for(int i=0;i<n-1;i++)
     {
-        for(int j=i+1;j<n;j++)
+        for(int j=0;j<n-i-1;j++)
         {
-            if(Q[i]<Q[j])
+            if(Q[j]<Q[j+1])
             {
-                temp = Q[i];
-                Q[i] = Q[j];
-                Q[j] = temp;
+                temp = Q[j];
+                Q[j] = Q[j+1];
+                Q[j+1] = temp;
                 flag=1;
             }
         }
